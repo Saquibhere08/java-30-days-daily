b30e051aa22ff48593e29e093cb2e9eeeedea094
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-03-09 23:06:34 +0530

    Add files via upload

---
+++ day2.java
@@ -0,0 +1,11 @@
+class day2
+{
+	public static void main(String [] arg)
+	{
+		int age=24;
+		String name = "Saquib Bin Halim";
+		String city = "Hyderbad";
+
+		System.out.println("My Name is " + name + ", I am " + age + ". I currently live in " + city + ".");	
+	}
+}
