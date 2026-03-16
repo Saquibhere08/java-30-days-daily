a1b86448a7a8016d11b0ac540781b720e037f529
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-03-16 21:57:11 +0530

    Add files via upload

---
+++ Narrow1.java
@@ -0,0 +1,21 @@
+class Narrow1
+{
+	public static void main(String [] args)
+	{	
+		double d = 40.75;
+		byte b=(byte) d;
+		short s = (short)d;
+		int a = (int)d;
+		long l = (long)d;
+		float f = (float)d;
+		
+		
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(l);
+		System.out.println(a);
+		System.out.println(s);
+		System.out.println(b);
+
+	}
+}
