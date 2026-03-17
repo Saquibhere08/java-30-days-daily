f2f708dcc3fa5d06bdc194c062a56024830bd3e1
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-03-17 17:50:34 +0530

    Add files via upload

---
+++ day7.java
@@ -0,0 +1,11 @@
+class day7{
+	public static void main(String[] args)
+	{
+		int a=100;
+		int b=9;
+
+		int res= a>b?a:b; //Using ternary Operator
+						
+		System.out.println("Largest Number among 2: "+res);
+	}
+}
