7001247d5b98cbe37506d64e628c4f0003a80b8a
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-03-19 22:15:06 +0530

    Add files via upload

---
+++ day8.java
@@ -0,0 +1,25 @@
+class day8
+{
+	public static void main(String[] arg)
+	{
+		//Simple Calculator
+
+		int a=22;
+		int b=55;
+		
+		int sum=a+b;
+
+		int mul=a*b;
+		
+		int div=a/b;
+	
+		int mod=a%b;
+
+		System.out.println(sum);
+		System.out.println(mul);
+		System.out.println(div);
+		System.out.println(mod);
+
+		
+	}
+}
