b8342ed380419f3cdfd01cbe2569bdbe2c76799d
Saquib Bin Halim <halimsaquib99@gmail.com>
2026-03-27 14:09:05 +0530

    Add files via upload

---
+++ Lday1.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class Lday1 {
+    // Print numbers from 1 to 100 using a loop.
+    public static void main(String [] arg){
+        fun();
+    }
+    public static void fun(){
+        for(int i=1;i<=100;i++){
+            System.out.println(i);
+        }
+    }
+}
